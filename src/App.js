a72e3c33ad14cc06d205944c0d7e04c3488f5c34
Himanshu Singh <hsnice16@gmail.com>
2022-06-24 21:18:26 +0530

    Fix roc8 suggestions (#27)
    * fix: bugs
    
    * fix: get bookmark call
    
    * disabled search
    
    * fix: edit profile
    
    * feat: search functionality
    
    * docs: update README and COMMENTS
    
    * styles: text-overflow
    
    * fix: edited name
    
    * fix: edited profile pic
    
    * refactor: file delete
    
    * conflicts

--- src/App.js
+++ src/App.js
@@ -3,16 +3,7 @@ import { useSelector } from "react-redux";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { Box, Container, useMediaQuery } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-import {
-  Home,
-  Explore,
-  Landing,
-  Profile,
-  Bookmarks,
-  SinglePost,
-  FollowersFollowing,
-} from "pages";
+import { Home, Explore, Landing, Profile, Bookmarks, SinglePost } from "pages";
 
 import {
   NotFound,
@@ -65,17 +56,10 @@ function App() {
                   <Route path={ROUTE_HOME} element={<Home />} />
                   <Route path={ROUTE_EXPLORE} element={<Explore />} />
                   <Route path={ROUTE_BOOKMARKS} element={<Bookmarks />} />
-
                   <Route
                     path={`${ROUTE_PROFILE}/:username`}
                     element={<Profile />}
-                  >
-                    <Route
-                      path=":followers_following"
-                      element={<FollowersFollowing />}
-                    />
-                  </Route>
-
+                  />
                   <Route
                     path={`${ROUTE_READ_POST}/:postId`}
                     element={<SinglePost />}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 export { Bookmarks } from "./bookmarks/Bookmarks";
 export { Explore } from "./explore/Explore";
-export { FollowersFollowing } from "./followers-following/FollowersFollowing";
 export { Home } from "./home/Home";
 export { Landing } from "./landing/Landing";
 export { Profile } from "./profile/Profile";
